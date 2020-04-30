ec1725bead7419e89a6844fa911736b2e4889f4c
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-04-30 16:54:13 +0200

    enregistrer les modi

--- codevores.js
+++ codevores.js
@@ -1,19 +1,26 @@
-<script>
-           /*La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli
-            *et d'appliquer les styles de validation aux différents éléments de formulaire*/
-           (function() {
-             'use strict';
-             window.addEventListener('load', function() {
-               let forms = document.getElementsByClassName('needs-validation');
-               let validation = Array.prototype.filter.call(forms, function(form) {
-                 form.addEventListener('submit', function(event) {
-                   if (form.checkValidity() === false) {
-                     event.preventDefault();
-                     event.stopPropagation();
-                   }
-                   form.classList.add('was-validated');
-                 }, false);
-               });
-             }, false);
-           })();
-         </script>
+
+/*La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli*/
+/*et d'appliquer les styles de validation aux différents éléments de formulaire*/
+  (function(){
+  'use strict';
+  window.addEventListener('load', function() {
+  let forms = document.getElementsByClassName('needs-validation');
+  let validation = Array.prototype.filter.call(forms, function(form) {
+  form.addEventListener('submit', function(event) {
+  if (form.checkValidity() === false) {
+  event.preventDefault();
+ event.stopPropagation();
+  }
+  form.classList.add('was-validated');
+  }, false);
+  });
+  }, false);
+})();
+
+
+/*bouton de menu de navigation*/
+const burger=document.querySelector('.burger');
+burger.addEventListener('click',() => {
+  burger.classList.toggle('active');
+});
+/*fin du menu de navigation*/
